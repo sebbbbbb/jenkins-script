91e6fd8540350d9dd3e0b158577ee143777a0279
Sébastien Pécoul <specoul@steamulo.com>
2019-02-06 17:13:52 +0100

    [ADD] On utilise des try-catch pour le build

--- vars/build.groovy
+++ vars/build.groovy
@@ -3,61 +3,67 @@
 def call(params) {
 
   def utils = new org.ftv.Utils()
-
-  stage("Check params"){
-    def mandatoryParams = ["project_name", "simulator", "slack_token", "slack_channel"]
-    mandatoryParams.each {
-      if ( params[{$it}] == null ) {
-        echo "Le paramètre ${it} doit être renseigné dans le tableau params."
-        assert false
+  try {
+    
+    stage("Check params"){
+      def mandatoryParams = ["project_name", "simulator", "slack_token", "slack_channel"]
+      mandatoryParams.each {
+        if ( params[{$it}] == null ) {
+          echo "Le paramètre ${it} doit être renseigné dans le tableau params."
+          assert false
+        }
       }
     }
-  }
 
-  stage("Checkout") {
-    checkout scm
-  }
+    stage("Checkout") {
+      checkout scm
+    }
 
-  stage("Clean") {
-    sh utils.cleanProjectCmd(params["project_name"])
-  }
+    stage("Clean") {
+      sh utils.cleanProjectCmd(params["project_name"])
+    }
 
-  stage("Bundler") {
-    sh utils.gemBundleInstallCmd()
-  }
+    stage("Bundler") {
+      sh utils.gemBundleInstallCmd()
+    }
 
-  stage("Cocoapods") {
-    sh utils.podInstallCmd()
-  }
+    stage("Cocoapods") {
+      sh utils.podInstallCmd()
+    }
 
-  stage("Test") {
-    sh 'set -o pipefail && env NSUnbufferedIO=YES'
-    sh "xcodebuild -workspace "+ params["project_name"] + ".xcworkspace \
-	      -scheme "+  params["project_name"] " \
-	      -configuration Debug \
-	      -destination 'platform=" + params["simulator"] + "'\
-	      -derivedDataPath './Build/Test_output/' \
-	      -enableCodeCoverage YES clean build test"
+    stage("Test") {
+      sh 'set -o pipefail && env NSUnbufferedIO=YES'
+      sh "xcodebuild -workspace "+ params["project_name"] + ".xcworkspace \
+          -scheme "+  params["project_name"] " \
+          -configuration Debug \
+          -destination 'platform=" + params["simulator"] + "'\
+          -derivedDataPath './Build/Test_output/' \
+          -enableCodeCoverage YES clean build test"
 
-    sh 'xsltproc -o ./Build/Test_output/report.junit ./scripts/plist_to_junit.xsl ./Build/Test_output/Logs/Test/*.xcresult/TestSummaries.plist'
-  }
+      sh 'xsltproc -o ./Build/Test_output/report.junit ./scripts/plist_to_junit.xsl ./Build/Test_output/Logs/Test/*.xcresult/TestSummaries.plist'
+    }
 
-  stage("SwiftLint") {
-    sh 'mkdir ./Build/Swiftlint_output'
-    def cmd = "swiftlint lint "
-    if ( params["lint_file"] != null ) {
-      cmd+= "--config " + params["lint_file"] + " "
-    } 
-    cmd+= "--reporter checkstyle  > ./Build/Swiftlint_output/report.xml"
-    sh cmd
-  }
+    stage("SwiftLint") {
+      sh 'mkdir ./Build/Swiftlint_output'
+      def cmd = "swiftlint lint "
+      if ( params["lint_file"] != null ) {
+        cmd+= "--config " + params["lint_file"] + " "
+      } 
+      cmd+= "--reporter checkstyle  > ./Build/Swiftlint_output/report.xml"
+      sh cmd
+    }
 
-  stage("Archive results") {
-    step([$class: 'JUnitResultArchiver', testResults: 'Build/Test_output/report.junit'])
-    step([$class: 'CheckStylePublisher', canComputeNew: false, defaultEncoding: '', healthy: '', pattern: 'Build/Swiftlint_output/report.xml', unHealthy: ''])
-  }
+    stage("Archive results") {
+      step([$class: 'JUnitResultArchiver', testResults: 'Build/Test_output/report.junit'])
+      step([$class: 'CheckStylePublisher', canComputeNew: false, defaultEncoding: '', healthy: '', pattern: 'Build/Swiftlint_output/report.xml', unHealthy: ''])
+    }
 
-  stage("Slack") {
-       slackSend(channel: params["slack_channel"], color: "#42f442", message: "Build de test executée avec succès.", token: params["slack_token"])
+    stage("Slack") {
+        slackSend(channel: params["slack_channel"], color: "#42f442", message: "Build de test executée avec succès.", token: params["slack_token"])
+    }
+
+  } catch(any) {
+    slackSend(channel: params["slack_channel"], color: "#d31010", message: "Failure" + any, token: params["slack_token"])
+    throw any //rethrow exception to prevent the build from proceeding
   }
 }
